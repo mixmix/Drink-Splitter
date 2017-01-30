9a51b04584b7f5b1d60f339db1e77271552e11c1
mix irving <whimful@gmail.com>
2017-01-30 14:26:22 +1300

    simple rewrite

--- src/components/whosPaying.js
+++ src/components/whosPaying.js
@@ -3,6 +3,11 @@ const { connect } = require('react-redux')
 const { Link } = require('react-router')
 const { FlatButton } = require('material-ui')
 
+// functional component / stateless component
+
+const WhosPaying = (props) => {
+  return (<div>Hello</div>)
+}
 
 const WhosPaying = (props) => {
   const { currentNight, dispatch } = props
@@ -13,12 +18,97 @@ const WhosPaying = (props) => {
       : 'white'
     return users[userKey]
   })
+
   return (
     <div>
     <h2>Who is going to pick up the bill?</h2>
-    {usersForDisplay.map(user => <FlatButton key={user.id} style={{backgroundColor: user.color}} onClick={() => {dispatch({type: 'USER_PAYING', payload: user.id})}}>{user.name}</FlatButton>)}
+    {usersForDisplay.map(user => {
+      return (
+        <FlatButton 
+          key={user.id}
+          style={{backgroundColor: user.color}}
+          onClick={() => { dispatch({type: 'USER_PAYING', payload: user.id}) } 
+        >
+         {user.name}
+        </FlatButton>
+      )
+    )}}
     </div>
   )
+}
+
+const WhosPaying = React.createClass({
+  render: function () {
+
+    return (<div>Hello</div>)
+  }
+
+})
+
+const WhosPaying = React.createClass({
+
+  // why can't we use fat arrow?
+  render: function () {
+    const { currentNight, dispatch } = this.props  // have to use this.props
+    const { users } = currentNight
+    const usersForDisplay = Object.keys(users).map(userKey => {
+      users[userKey].color = users[userKey].paying
+        ? 'green'
+        : 'white'
+      return users[userKey]
+    })
+
+    return (
+      <div>
+        <h2>Who is going to pick up the bill?</h2>
+        {usersForDisplay.map(user => {
+          return (
+            <FlatButton 
+              key={user.id}
+              style={{backgroundColor: user.color}}
+              onClick={() => { dispatch({type: 'USER_PAYING', payload: user.id}) } 
+            >
+             {user.name}
+            </FlatButton>
+          )
+        )}}
+      </div>
+    )
+  }
+})
+
+
+class WhosPaying extends React.Component {
+
+  // methods are functions which belong to a particular instance (or copy of) a component
+
+  render () {
+    const { currentNight, dispatch } = this.props  // have to use this.props
+    const { users } = currentNight
+    const usersForDisplay = Object.keys(users).map(userKey => {
+      users[userKey].color = users[userKey].paying
+        ? 'green'
+        : 'white'
+      return users[userKey]
+    })
+
+    return (
+      <div>
+        <h2>Who is going to pick up the bill?</h2>
+        {usersForDisplay.map(user => {
+          return (
+            <FlatButton 
+              key={user.id}
+              style={{backgroundColor: user.color}}
+              onClick={() => { dispatch({type: 'USER_PAYING', payload: user.id}) } 
+            >
+             {user.name}
+            </FlatButton>
+          )
+        )}}
+      </div>
+    )
+  }
 }
 
 
